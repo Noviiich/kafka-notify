6f6ff8f8ff6a3cf79284e243b5bc613edecd647b
Noviiich <nowikoff63@gmail.com>
2025-06-18 18:56:14 +0300

    add: Создание точки входа в приложений producer

---
+++ cmd/producer/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Noviiich/kafka-notify/internal/config"
+	"github.com/Noviiich/kafka-notify/internal/http-server/handlers/notification/send"
+	mwLogger "github.com/Noviiich/kafka-notify/internal/http-server/middleware/logger"
+	sendService "github.com/Noviiich/kafka-notify/internal/http-server/services/notification/send"
+	"github.com/Noviiich/kafka-notify/internal/kafka/producer"
+	"github.com/Noviiich/kafka-notify/internal/lib/logger/handlers/slogpretty"
+	"github.com/Noviiich/kafka-notify/internal/lib/logger/sl"
+	"github.com/Noviiich/kafka-notify/internal/storage/sqlite"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info(
+		"starting consumer",
+		slog.String("env", cfg.Env),
+		slog.String("version", "v0.0.1"),
+	)
+	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	log.Info("starting db")
+
+	kafkaProducer, err := producer.New(cfg.KafkaConfig)
+	if err != nil {
+		log.Error("failed to initialize kafka producer %s", sl.Err(err))
+		os.Exit(1)
+	}
+	defer kafkaProducer.Close()
+	log.Info("starting producer")
+
+	sendNotificationService := sendService.New(storage, kafkaProducer)
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID) // Добавляет request_id в каждый запрос, для трейсинга
+	router.Use(middleware.Logger)    // Логирование всех запросов
+	router.Use(mwLogger.New(log))    // внутренний логгер
+	router.Use(middleware.Recoverer) // Если где-то внутри сервера (обработчика запроса) произойдет паника, приложение не должно упасть
+	router.Use(middleware.URLFormat) // Парсер URLов поступающих запросов
+
+	router.Post("/send", send.New(log, sendNotificationService))
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	// Graceful shutdown
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Error("failed to start server")
+		}
+	}()
+
+	log.Info("server started")
+
+	<-done
+	log.Info("stopping server")
+
+	// TODO: move timeout to config
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server", sl.Err(err))
+
+		return
+	}
+
+	// TODO: close storage
+
+	log.Info("server stopped")
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default: // If env config is invalid, set prod settings by default due to security
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,47 @@
 module github.com/Noviiich/kafka-notify
 
 go 1.23.4
+
+require (
+	github.com/fatih/color v1.18.0
+	github.com/mattn/go-sqlite3 v1.14.28
+)
+
+require (
+	github.com/ajg/form v1.5.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/IBM/sarama v1.45.2
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.7.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/go-chi/render v1.0.3
+	github.com/golang-migrate/migrate/v4 v4.18.3
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.6.0
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
