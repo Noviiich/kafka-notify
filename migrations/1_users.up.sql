1c8d47c51b9f0e99e7443dde634912185538c055
Noviiich <nowikoff63@gmail.com>
2025-06-19 07:19:41 +0300

    feat: Добавление миграций для notifications

--- migrations/1_users.up.sql
+++ migrations/1_users.up.sql
@@ -3,3 +3,13 @@ CREATE TABLE IF NOT EXISTS users
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS notifications (
+    id TEXT PRIMARY KEY,
+    from_user_id INTEGER NOT NULL,
+    to_user_id INTEGER NOT NULL,
+    message TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (from_user_id) REFERENCES users(id),
+    FOREIGN KEY (to_user_id) REFERENCES users(id)
+);
---
+++ migrations/2_insert_users.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, name) VALUES
+    (1, 'Emma'),
+    (2, 'Bruno'),
+    (3, 'Rick'),
+    (4, 'Lena');
