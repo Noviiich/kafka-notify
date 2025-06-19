1923e892ddbc4f8a692a64c4411412e44de8b4eb
Noviiich <nowikoff63@gmail.com>
2025-06-19 07:21:00 +0300

    feat: Создание точки входа для работы consumer и ручки

---
+++ cmd/consumer/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/Noviiich/kafka-notify/internal/config"
+	"github.com/Noviiich/kafka-notify/internal/http-server/handlers/notification/process"
+	mwLogger "github.com/Noviiich/kafka-notify/internal/http-server/middleware/logger"
+	"github.com/Noviiich/kafka-notify/internal/kafka/consumer"
+	"github.com/Noviiich/kafka-notify/internal/lib/logger/handlers/slogpretty"
+	"github.com/Noviiich/kafka-notify/internal/lib/logger/sl"
+	"github.com/Noviiich/kafka-notify/internal/storage/sqlite"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info(
+		"starting consumer",
+		slog.String("env", cfg.Env),
+		slog.String("version", "v0.0.1"),
+	)
+	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	log.Info("starting db")
+
+	consumer, err := consumer.New(cfg.KafkaConfig, storage)
+	if err != nil {
+		log.Error("failed to init kafka consumer", sl.Err(err))
+		os.Exit(1)
+	}
+	log.Info("kafka consumer initialized", slog.String("topic", cfg.KafkaConfig.Topic))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := consumer.Start(ctx); err != nil {
+			log.Error("kafka consumer error", sl.Err(err))
+		}
+	}()
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID) // Добавляет request_id в каждый запрос, для трейсинга
+	router.Use(middleware.Logger)    // Логирование всех запросов
+	router.Use(mwLogger.New(log))    // внутренний логгер
+	router.Use(middleware.Recoverer) // Если где-то внутри сервера (обработчика запроса) произойдет паника, приложение не должно упасть
+	router.Use(middleware.URLFormat) // Парсер URLов поступающих запросов
+	router.Get("/{userID}", process.New(log, storage))
+
+	// Graceful shutdown
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := http.Server{
+		Addr:    cfg.Consumer.Address,
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Error("failed to start server")
+		}
+	}()
+
+	log.Info("server started")
+
+	<-done
+	log.Info("stopping server")
+
+	// TODO: move timeout to config
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server", sl.Err(err))
+
+		return
+	}
+
+	// TODO: close storage
+
+	log.Info("server stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default: // If env config is invalid, set prod settings by default due to security
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
